f4535e5989b57e99c5b35dbc3fada646f8695ef6
eddiedigits <eddiedigits@gmail.com>
2024-05-08 09:48:59 +0200

    improved doc strings

--- cchub_api_base_client.py
+++ cchub_api_base_client.py
@@ -25,23 +25,25 @@ def set_get_all_method(cls):
 @set_get_all_method
 @set_model_method
 class CchubApiBaseClient:
-    '''basic class to act as a client for the CCHUB Api
-    it is expected that this Class will be extended with business logic
-    for middleware purposes
+    """
+    A basic class to act as a client for the CCHUB API.
 
-    scope:
-    handle auth, session and retry functionality
-    provide basic methods for the api endpoints
+    This class is expected to be extended with business logic for middleware purposes.
 
-    added dynamic methods:
-    self.get_{model} 
-        self.get_accounts() # get first page. default pagesize is 100
-        self.get_accounts('name_4385245245') # get single item
-    self.post_{model}(json={...}) # add single item
-    self.put_{model}(cchub_uid, json={...}) # update single item
-    self.get_all_{model} # get all items
+    Scope:
+    - Handles authentication, session, and retry functionality.
+    - Provides basic methods for the API endpoints.
 
-    json: {
+    Added dynamic methods:
+    - self.get_{model}
+        - self.get_accounts()  # get first page. Default pagesize is 100
+        - self.get_accounts('name_4385245245')  # get single item
+    - self.post_{model}(json={...})  # add single item
+    - self.put_{model}(cchub_uid, json={...})  # update single item
+    - self.get_all_{model}  # get all items
+
+    JSON structure:
+    {
         'title': 'string',
         'database': {
             'title': 'string'
@@ -51,30 +53,31 @@ class CchubApiBaseClient:
         },
     }
 
-    params:
+    Parameters:
+    - json (dict): The JSON data to be sent in the request body.
+
+    Params:
     {
-        'skip': 0, # offset
-        'take': 20, # pagesize
+        'skip': 0,  # offset
+        'take': 20,  # pagesize
         'sort': [
-		    {"field":"firstname", "dir":"desc"},
-		    {"field":"lastname", "dir":"asc"}
-        ], # custom field example: "field": "customFields.telefon"
+            {"field": "firstname", "dir": "desc"},
+            {"field": "lastname", "dir": "asc"}
+        ],  # custom field example: "field": "customFields.telefon"
         "filter": {
-		    "field": "firstname",
-		    "operator": "eq", # various operators available
-		    "value": "John"
-	    }, # filter can be a list of filter dicts
+            "field": "firstname",
+            "operator": "eq",  # various operators available
+            "value": "John"
+        },  # filter can be a list of filter dicts
         "fields": [
             "firstname",
             "lastname",
             "account.title"
-	    ], # limit the fields which are returned
-
+        ],  # limit the fields which are returned
     }
 
-    response.status_code: 200
-    response.url: api_endpoint
-    response.json(): {
+    Response structure:
+    {
         "error": [],
         "result": {
             "data": [
@@ -90,10 +93,9 @@ class CchubApiBaseClient:
             ],
             "total": 0
         },
-        
     }
-    '''
-
+    """
+    # There are dynamic methods for each model
     models = [
         'activities',
         'activitiesCall',
@@ -117,121 +119,213 @@ class CchubApiBaseClient:
         'statuses',
         'templates',
         'tickets',
-        'users',]
+        'users',
+    ]
 
     def __init__(self, server_address, token, api_version='6'):
+        """
+        Initialize the CchubApiBaseClient.
+
+        Parameters:
+        - server_address (str): The address of the CCHUB server.
+        - token (str): The access token for authentication.
+        - api_version (str): The version of the API to use (default is '6').
+        """
         self.base_url = f'{server_address}'
         self.access_token = token
         self.version_url = f'/api/v{api_version}'
         self.session = Session()
         adapter = HTTPAdapter(max_retries=3)
         self.session.mount(self.base_url, adapter)
-        self.session.headers["Content-Type"]= "application/json"
+        self.session.headers["Content-Type"] = "application/json"
         self.timeout = 10
 
     def model_func(self, model, verb, *args, **kwargs):
-        '''simple get request for a model'''
-        # cc.put_accounts('123', json={'customFields': {'business_name': 'test'}})
-        # cc.get_accounts('123')
-        # cc.get_accounts(params={'skip': 0, 'take': 20})
-        uid = args[0] if args else None
-        simulate = kwargs.pop('simulate', False)
+            """
+            Perform a HTTP request for a model using the specified verb.
 
-        if uid:
-            endpoint = f'{self.version_url}/{model}/{uid}.json'
-        else:
-            endpoint = f'{self.version_url}/{model}.json'
+            This function is used by the decorator to dynamically create methods for HTTP verbs for each model.
+            For example, `self.get_activities()` or `self.post_account(json)`.
 
-        verb_dict = {
-            'get': self.get,
-            'post': self.post,
-            'put': self.put,
-            'delete': self.delete,
-        }
-        if simulate:
-            return self.simulate(endpoint, verb, **kwargs)
-        else:
-            return verb_dict[verb](endpoint, **kwargs)
+            Parameters:
+            - model (str): The name of the model.
+            - verb (str): The HTTP verb to use for the request.
+            - args: Positional arguments for the request.
+            - kwargs: Keyword arguments for the request.
+
+            Returns:
+            - object: The response object.
+
+            Raises:
+            - KeyError: If the specified verb is not supported.
+
+            """
+            uid = args[0] if args else None
+            simulate = kwargs.pop('simulate', False)
+
+            if uid:
+                endpoint = f'{self.version_url}/{model}/{uid}.json'
+            else:
+                endpoint = f'{self.version_url}/{model}.json'
+
+            verb_dict = {
+                'get': self.get,
+                'post': self.post,
+                'put': self.put,
+                'delete': self.delete,
+            }
+            if simulate:
+                return self.simulate(endpoint, verb, **kwargs)
+            else:
+                return verb_dict[verb](endpoint, **kwargs)
 
     def _auth(self, params=None):
-        '''add access token to params variable'''
+        """
+        Add the access token to the params variable.
+
+        Parameters:
+        - params (dict): The parameters to be sent in the request.
+
+        Returns:
+        - dict: The updated parameters with the access token.
+        """
         if params:
             params['accessToken'] = self.access_token
-        else: 
+        else:
             params = {'accessToken': self.access_token}
         return params
-    
+
     def _make_request(self, method, endpoint, **kwargs):
-        # prep vars
+        """
+        Make a request to the API.
+
+        Parameters:
+        - method (str): The HTTP method to use for the request.
+        - endpoint (str): The API endpoint to send the request to.
+        - kwargs: Keyword arguments for the request.
+
+        Returns:
+        - object: The response object.
+        """
         params = kwargs.get('params', None)
         try:
             del kwargs['params']
         except KeyError:
             pass
-        params=self._auth(params)
+        params = self._auth(params)
         param_string = Php.http_build_query(params)
         url = f'{self.base_url}{endpoint}?{param_string}'
-        # prep request
         req = Request(
             method,
             url,
-            # params=self._auth(params),
             **kwargs)
         prepped = req.prepare()
-        
+
         try:
             response = self.session.send(prepped, timeout=self.timeout)
-            # response.raise_for_status()  # Raise an exception for HTTP errors
             return response
         except RequestException as error:
             print(f"Error fetching data from the API: {error}")
             return None
 
     def get(self, endpoint, **kwargs):
+        """
+        Perform a GET request.
+
+        Parameters:
+        - endpoint (str): The API endpoint to send the request to.
+        - kwargs: Keyword arguments for the request.
+
+        Returns:
+        - object: The response object.
+        """
         return self._make_request('GET', endpoint, **kwargs)
 
     def post(self, endpoint, **kwargs):
+        """
+        Perform a POST request.
+
+        Parameters:
+        - endpoint (str): The API endpoint to send the request to.
+        - kwargs: Keyword arguments for the request.
+
+        Returns:
+        - object: The response object.
+        """
         return self._make_request('POST', endpoint, **kwargs)
 
     def put(self, endpoint, **kwargs):
+        """
+        Perform a PUT request.
+
+        Parameters:
+        - endpoint (str): The API endpoint to send the request to.
+        - kwargs: Keyword arguments for the request.
+
+        Returns:
+        - object: The response object.
+        """
         return self._make_request('PUT', endpoint, **kwargs)
 
     def delete(self, endpoint, **kwargs):
-        return self._make_request('DELETE', endpoint, **kwargs)
-    
-    def simulate(self, endpoint, method, params=None, **kwargs):
-        if params:
-            params['_method'] = method.upper()
-        else: 
-            params = {'_method': method.upper()}
-        return self._make_request('GET', endpoint, params=params, **kwargs)
-    
-    def get_all(self, model, **kwargs):
         """
-        Retrieve data from an API with paging support.
+        Perform a DELETE request.
 
         Parameters:
-        - params (dict): Optional parameters to include in the API request.
+        - endpoint (str): The API endpoint to send the request to.
+        - kwargs: Keyword arguments for the request.
 
         Returns:
-        - list: A list of all items retrieved from the API.
+        - object: The response object.
+        """
+        return self._make_request('DELETE', endpoint, **kwargs)
+
+    def simulate(self, endpoint, method, params=None, **kwargs):
+        """
+        Simulate a request.
+
+        Parameters:
+        - endpoint (str): The API endpoint to send the request to.
+        - method (str): The HTTP method to use for the request.
+        - params (dict): The parameters to be sent in the request.
+        - kwargs: Keyword arguments for the request.
+
+        Returns:
+        - object: The response object.
+        """
+        if params:
+            params['_method'] = method.upper()
+        else:
+            params = {'_method': method.upper()}
+        return self._make_request('GET', endpoint, params=params, **kwargs)
+
+    def get_all(self, model, **kwargs):
+        """
+        Retrieve data from the API with paging support.
+
+        Parameters:
+        - model (str): The name of the model.
+        - kwargs: Keyword arguments for the request.
+
+        Returns:
+        - dict: A dictionary containing the retrieved data.
         """
         all_data = {
             'result': {
                 'data': [],
                 'total': 0,
-                },
+            },
             'error': [],
             'status_code': [],
         }
         position = 0
 
-        # find the method to use
+        # Find the method to use
         do = f'get_{model}'
         if hasattr(self, do) and callable(func := getattr(self, do)):
             method = func
         else:
-            raise Exception(f'no method for {do}')
+            raise Exception(f'No method for {do}')
 
         # Loop until all pages have been retrieved
         while True:
@@ -256,7 +350,7 @@ class CchubApiBaseClient:
             res = response.json()
             all_data['error'].append(res['error'])
             all_data['result']['data'].extend(res['result']['data'])
-            # get total from first page
+            # Get total from the first page
             if position == 0:
                 all_data['result']['total'] = res['result']['total']
 
